c1ed4c000f573b10c27f7de7bab5b20af1807ea6
Hori2000 <it.david.horvath@gmail.com>
2017-02-27 06:37:08 -0800

    Conflict B

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -16,7 +16,7 @@ public class TrainControllerImpl implements TrainController {
 			referenceSpeed += step;
 		}
 
-		if (referenceSpeed < 10)
+		if (referenceSpeed < 0)
 			referenceSpeed = 0;
 		enforceSpeedLimit();
 	}
